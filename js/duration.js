ef98289b1df5be042e76f920932ed5219ef239a0
Saltyfishwithoutdream <1311156445@qq.com>
2021-09-22 00:35:12 +0800

    Site updated: 2021-09-22 00:35:07

--- js/duration.js
+++ js/duration.js
@@ -22,7 +22,7 @@
       if(String(snum).length === 1 ){
         snum = "0" + snum;
       }
-      document.getElementById("timeDate").innerHTML = "本站安全运行&nbsp"+dnum+"&nbsp天";
+      document.getElementById("timeDate").innerHTML = "本站托王老师的福,已经在夹缝中运行了&nbsp"+dnum+"&nbsp天";
       document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
   
